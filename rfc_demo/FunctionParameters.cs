4cacea7c64cd0e9ccf1ad49a02c9bae320fcd503
Nightstars <ChristZhangowner@gmail..com>
2020-09-17 20:52:06 +0800

    mirror bug fix

--- rfc_demo/FunctionParameters.cs
+++ rfc_demo/FunctionParameters.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* -----------------------------------------------------------------------------
+ 1.    INTRODUCTION
+This is an enlightenment program based on the SapNwRfc project, about how
+to call RFC in .net core.  You can modify and distribute copies of this
+program at will, but there is one thing you must do and acknowledge : 
+No matter what purpose you use, you must indicate the source of this code.
+star and fork will be better
+2.    CONTACT INFORMATION
+chrsitzhangowner@gmail.com
+----------------------------------------------------------------------------- */
+
+/******************* rfc .net core support ********************
+   Author(s):   christ chang
+   Description: rfc .net core support
+   github: https://github.com/Nightstars/rfc_demo
+   thanks: huysentruitw and his work 
+*******************************************************************************/
+using System;
 using System.Collections.Generic;
 using System.Text;
 using SapNwRfc;
--- rfc_demo/Program.cs
+++ rfc_demo/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/* -----------------------------------------------------------------------------
+ 1.    INTRODUCTION
+This is an enlightenment program based on the SapNwRfc project, about how
+to call RFC in .net core.  You can modify and distribute copies of this
+program at will, but there is one thing you must do and acknowledge : 
+No matter what purpose you use, you must indicate the source of this code.
+star and fork will be better
+2.    CONTACT INFORMATION
+chrsitzhangowner@gmail.com
+----------------------------------------------------------------------------- */
+
+/******************* rfc .net core support ********************
+   Author(s):   christ chang
+   Description: rfc .net core support
+   github: https://github.com/Nightstars/rfc_demo
+   thanks: huysentruitw and his work 
+*******************************************************************************/
+using System;
 using SapNwRfc;
 namespace rfc_demo
 {
@@ -8,10 +25,15 @@ namespace rfc_demo
         {
             try
             {
+                //define connect string 
                 string connectionString = "AppServerHost=192.168.95.20; SystemNumber=00; User=ZBT_RFC; Password=Zbt_rfc123; Client=310; Language=ZH; PoolSize=5; Trace=8;";
+                //structure rfc connection
                 using var connection = new SapConnection(connectionString);
+                //open rfc connection
                 connection.Connect();
+                //structure call function by rfc target fun
                 using var someFunction = connection.CreateFunction("ZMDG_MDATA_INFO");
+                //call remote rfc functon with input parameter constructor and get fun result
                 rfc_demo.FunctionResult result= someFunction.Invoke<FunctionResult>(new FunctionParameters
                 {
                     Ibukrs = "1010",
@@ -19,6 +41,7 @@ namespace rfc_demo
                     Izdata = "20200801",
                     Iztime = "00:00:00"
                 });
+                //print result in console 
                 foreach(ZMDGS003 item in result.result)
                 {
                     Console.WriteLine(item.Mdata);
